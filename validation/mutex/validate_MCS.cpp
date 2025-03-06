adbf1b8b0f25d53c88b276001cea3297acd48ed2
Nguyễn Trần Phước Thành <ntpthanh.sdh242@hcmut.edu.vn>
2025-03-06 12:39:34 +0700

    Add validation for spinlock mutex types

---
+++ validation/mutex/validate_MCS.cpp
@@ -0,0 +1,75 @@
+#include <implementation/mutex_mcs.hpp>
+#include <iostream>
+#include <thread>
+#include <vector>
+
+constexpr int TEST_ITER = 500;
+constexpr int NUM_THREAD = 4;  // should be set of max thread count on system,
+                               // higher -> lock waiter preemption problem -> REALLY bad perf
+constexpr int INCREMENT_AMOUNT_PER_THREAD = 10000;
+
+int counter = 0;
+Spinlock_Mutex_MCS mtx;
+
+void increment_thread_without_lock(int iterations)
+{
+    for (int j = 0; j < iterations; ++j)
+    {
+        counter++;
+    }
+}
+
+void increment_thread_with_lock(int iterations)
+{
+    auto n = Spinlock_Mutex_MCS::QueueNode();
+    for (int j = 0; j < iterations; ++j)
+    {
+        // Manual mutex locking and unlocking
+        mtx.lock(&n);
+        counter++;
+        mtx.unlock(&n);
+    }
+}
+
+bool test_run_increments(int num_threads, int iterations_per_thread, void increment_function(int))
+{
+    counter = 0;
+    std::vector<std::thread> threads;
+
+    for (int i = 0; i < num_threads; ++i)
+    {
+        threads.emplace_back(increment_function, iterations_per_thread);
+    }
+
+    for (auto &thread : threads)
+    {
+        thread.join();
+    }
+
+    return counter == (num_threads * iterations_per_thread);
+}
+
+int main()
+{
+
+    int success_count_without_lock = 0;
+    int success_count_with_lock = 0;
+    for (int i = 0; i < TEST_ITER; i++)
+    {
+        success_count_without_lock += test_run_increments(NUM_THREAD, INCREMENT_AMOUNT_PER_THREAD,
+                                                          increment_thread_without_lock);
+        success_count_with_lock += test_run_increments(NUM_THREAD, INCREMENT_AMOUNT_PER_THREAD,
+                                                       increment_thread_with_lock);
+    }
+
+    std::cout << "Test result:" << '\n'
+              << "Without lock: " << success_count_without_lock << '/' << TEST_ITER << '\n'
+              << "With lock: " << success_count_with_lock << '/' << TEST_ITER << '\n';
+
+    if (success_count_with_lock == TEST_ITER)
+        std::cout << "[PASSED]" << std::endl;
+    else
+        std::cout << "[FAILED]" << std::endl;
+
+    return 0;
+}
